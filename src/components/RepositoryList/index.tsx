c25ada457d7da997049e7bdbf3fd7d8e8570d0d9
brenoos <breno.oliver07@gmail.com>
2019-07-02 00:13:43 -0300

    exemplos de typescript com FC e useState

---
+++ src/components/RepositoryList/index.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+interface Repository {
+  id: number;
+  name: string;
+}
+
+interface Props {
+  repositories: Repository[];
+}
+
+
+const RepositoryList: React.FC<Props> = ({ repositories }) => {
+    const [newRepository, setNewRepository] = useState("");
+
+
+    return <ul>{repositories.map(repository => <li>{ repository.name }</li> )}</ul>;
+};
+
+export default RepositoryList;
