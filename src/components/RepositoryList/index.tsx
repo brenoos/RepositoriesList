c32dc363591ac47afce586b1185ea93322451e04
brenoos <breno.oliver07@gmail.com>
2019-07-03 00:59:24 -0300

    criado sagas e repositoryItem

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Dispatch } from 'redux';
 import { ApplicationState } from '../../store';
 import { Repository } from '../../store/ducks/repositories/types';
-import {
-  loadRequest,
-  loadSuccess,
-  loadFailure,
-} from '../../store/ducks/repositories/actions';
+import { loadRequest } from '../../store/ducks/repositories/actions';
+
+import RepositoryItem from '../RepositoryItem';
 
 const RepositoryList: React.FC = () => {
   const repositories = useSelector(
@@ -17,10 +15,14 @@ const RepositoryList: React.FC = () => {
 
   useEffect(() => {
     dispatch(loadRequest());
-  });
+  }, [dispatch]);
 
   return (
-    <ul>{repositories.map((repository: Repository) => repository.name)}</ul>
+    <ul>
+      {repositories.map((repository: Repository) => (
+        <RepositoryItem key={repository.id} repository={repository} />
+      ))}
+    </ul>
   );
 };
 
